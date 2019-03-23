227e38755af9baa14350b201b7abf5b9418ff247
BenjaminRA <benjamin.gra572@gmail.com>
2019-03-23 16:43:32 -0300

    initial commit

--- Lab 1/Lab_1_1.m
+++ Lab 1/Lab_1_1.m
@@ -8,20 +8,20 @@ dt = 1/sr;
 t = 0:dt:(length(y)*dt)-dt;
 
 delay_sec = 1;
+cantidad = 1;
 
 % Concatenar el vector de 0 al principio con la señal original para crear el
 % Efecto de Eco
 delay = (cat(2, zeros(1, floor(sr*delay_sec)), y'))';
 t_delay = 0:dt:(length(delay)*dt)-dt;
 
-fade = 1:-1/(sr*1):0;
-fade = [fade zeros(1, length(y)-(sr*1))];
+fade = 1:-1/length(y):0;
 for i = 1:length(y)
     delay(i + floor(sr*delay_sec)) = delay(i + floor(sr*delay_sec))*fade(i);
 end
 
-final = cat(2, y', zeros(1, floor(sr*delay_sec))) + delay';
-sound(delay, sr);
+final = cat(2, y', zeros(1, floor(sr*delay_sec))) + delay'.*cantidad;
+% sound(final, sr);
 x = 0:dt:(length(final)*dt)-dt;
 hold on;
 plot(x, delay);
